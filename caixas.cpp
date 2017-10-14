501af5abfff86f1208d81b3489de130d16e2eb5c
Marcela Ribeiro de Oliveira <mro15@inf.ufpr.br>
2017-10-13 23:47:06 -0300

    acabado algoritmo só falta imprimir do jeito que o professor quer

--- caixas.cpp
+++ caixas.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <cstdlib>
 #include <vector>
+#include <sstream>
+#include <string>
+#include <algorithm>
 
 typedef struct box{
 	int id;
 	int height;
 	int width;
 	int depth;
+	int area;
+	int lex_sort;
 }box;
+
+bool compare(box a, box b){
+    return  a.lex_sort > b.lex_sort;
+}
 
 int main(int argc, char *argv[]){
 	if(argc!=1){
@@ -19,12 +28,43 @@ int main(int argc, char *argv[]){
 	box dimensions;
 	while(std::cin >> dimensions.height >> dimensions.width >> dimensions.depth){
 		dimensions.id = id;
+		dimensions.area = dimensions.height * dimensions.width;
+		std::ostringstream aux;
+		aux << dimensions.width << dimensions.depth;
+		std::istringstream aux1(aux.str());
+		aux1 >> dimensions.lex_sort;
 		boxes.push_back(dimensions);
 		id++;
 	}
 	// Print vector [1...n]
-	for(std::vector<box>::iterator it = boxes.begin(); it != boxes.end(); ++ it){
-		std::cout << (*it).id << " " << (*it).height << " " << (*it).width << " " << (*it).depth <<  std::endl;
+	/*for(std::vector<box>::iterator it = boxes.begin(); it != boxes.end(); ++ it){
+		std::cout << (*it).id << " " << (*it).height << " " << (*it).width << " " << (*it).depth << " " << (*it).area << " " << (*it).lex_sort <<  std::endl;
+	}*/
+	// Sort decreasing by lex_sort
+	std::sort(boxes.begin(), boxes.end(), compare);	
+	/*for(std::vector<box>::iterator it = boxes.begin(); it != boxes.end(); ++ it){
+		std::cout << (*it).id << " " << (*it).height << " " << (*it).width << " " << (*it).depth << " " << (*it).area << " " << (*it).lex_sort <<  std::endl;
+	}*/
+	int n = id-1;
+	int pd_vector[n];
+	for (int i = 0; i < n; i++ ){
+     	pd_vector[i] = boxes.at(i).height;
+		std::cout << pd_vector[i] << std::endl;
 	}
+	for(int i=1; i<n; ++i){
+		for(int j = 0; j< i; ++j){
+			if(boxes.at(i).width < boxes.at(j).width && boxes.at(i).depth < boxes.at(j).depth && pd_vector[i] < pd_vector[j] + boxes.at(i).height){
+				pd_vector[i] = pd_vector[j] + boxes.at(i).height;
+			}
+		}
+	}
+	int max = -1;
+	for(int i=0; i<n; ++i){
+		if(max<pd_vector[i]){
+			max = pd_vector[i];
+		}
+	}
+	std::cout << max << std::endl; 
+	
 	return 0;
 }
